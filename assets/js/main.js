a6be047e0e381f51a40f35865164a67df2215933
ygwang <ygwangthu@gmail.com>
2020-03-09 22:04:56 +0800

    new style for mobile phone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,226 +1,153 @@
 (function($) {
 
-	var settings = {
+	var $window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$main = $('#main'),
+		$panels = $main.children('.panel'),
+		$nav = $('#nav'), $nav_links = $nav.children('a');
 
-		// Speed to resize panel.
-			resizeSpeed: 600,
-
-		// Speed to fade in/out.
-			fadeSpeed: 300,
-
-		// Size factor.
-			sizeFactor: 11.5,
-
-		// Minimum point size.
-			sizeMin: 15,
-
-		// Maximum point size.
-			sizeMax: 20
-
-	};
-
-	var $window = $(window);
-
-	$(document).ready(function(){
-	//$window.on('load', function() {
-
-		skel
-			.breakpoints({
-				desktop: '(min-width: 737px)',
-				mobile: '(max-width: 736px)'
-			})
-			.viewport({
-				breakpoints: {
-					desktop: {
-						width: 1080,
-						scalable: false
-					}
-				}
-			})
-			.on('+desktop', function() {
-
-				var	$body = $('body'),
-					$main = $('#main'),
-					$panels = $main.find('.panel'),
-					$hbw = $('html,body,window'),
-					$footer = $('#footer'),
-					$wrapper = $('#wrapper'),
-					$nav = $('#nav'), $nav_links = $nav.find('a'),
-					$jumplinks = $('.jumplink'),
-					panels = [],
-					activePanelId = null,
-					firstPanelId = null,
-					isLocked = false,
-					hash = window.location.hash.substring(1);
-
-				if (skel.vars.mobile) {
-
-					settings.fadeSpeed = 0;
-					settings.resizeSpeed = 0;
-					$nav_links.find('span').remove();
-
-				}
-
-				// Body.
-					$body._resize = function() {
-						var factor = ($window.width() * $window.height()) / (1440 * 900);
-						$body.css('font-size', Math.min(Math.max(Math.floor(factor * settings.sizeFactor), settings.sizeMin), settings.sizeMax) + 'pt');
-						$main.height(panels[activePanelId].outerHeight());
-						$body._reposition();
-					};
-
-					$body._reposition = function() {
-						if (skel.vars.mobile && (window.orientation == 0 || window.orientation == 180))
-							$wrapper.css('padding-top', Math.max((($window.height() - (panels[activePanelId].outerHeight() + $footer.outerHeight())) / 2) - $nav.height(), 30) + 'px');
-						else
-							$wrapper.css('padding-top', ((($window.height() - panels[firstPanelId].height()) / 2) - $nav.height()) + 'px');
-					};
-
-				// Panels.
-					$panels.each(function(i) {
-						var t = $(this), id = t.attr('id');
-
-						panels[id] = t;
-
-						if (i == 0) {
-
-							firstPanelId = id;
-							activePanelId = id;
-
-						}
-						else
-							t.hide();
-
-						t._activate = function(instant) {
-
-							// Check lock state and determine whether we're already at the target.
-								if (isLocked
-								||	activePanelId == id)
-									return false;
-
-							// Lock.
-								isLocked = true;
-
-							// Change nav link (if it exists).
-								$nav_links.removeClass('active');
-								$nav_links.filter('[href="#' + id + '"]').addClass('active');
-
-							// Change hash.
-								if (i == 0)
-									window.location.hash = '#';
-								else
-									window.location.hash = '#' + id;
-
-							// Add bottom padding.
-								var x = parseInt($wrapper.css('padding-top')) +
-										panels[id].outerHeight() +
-										$nav.outerHeight() +
-										$footer.outerHeight();
-
-								if (x > $window.height())
-									$wrapper.addClass('tall');
-								else
-									$wrapper.removeClass('tall');
-
-							// Fade out active panel.
-								$footer.fadeTo(settings.fadeSpeed, 0.0001);
-								panels[activePanelId].fadeOut(instant ? 0 : settings.fadeSpeed, function() {
-
-									// Set new active.
-										activePanelId = id;
-
-										// Force scroll to top.
-											$hbw.animate({
-												scrollTop: 0
-											}, settings.resizeSpeed, 'swing');
-
-										// Reposition.
-											$body._reposition();
-
-										// Resize main to height of new panel.
-											$main.animate({
-												height: panels[activePanelId].outerHeight()
-											}, instant ? 0 : settings.resizeSpeed, 'swing', function() {
-
-												// Fade in new active panel.
-													$footer.fadeTo(instant ? 0 : settings.fadeSpeed, 1.0);
-													panels[activePanelId].fadeIn(instant ? 0 : settings.fadeSpeed, function() {
-
-														// Unlock.
-															isLocked = false;
-
-													});
-											});
-
-								});
-
-						};
-
-					});
-
-				// Nav + Jumplinks.
-					$nav_links.add($jumplinks).click(function(e) {
-						var t = $(this), href = t.attr('href'), id;
-
-						if (href.substring(0,1) == '#') {
-
-							e.preventDefault();
-							e.stopPropagation();
-
-							id = href.substring(1);
-
-							if (id in panels)
-								panels[id]._activate();
-
-						}
-
-					});
-
-				// Window.
-					$window
-						.resize(function() {
-
-							if (!isLocked)
-								$body._resize();
-
-						});
-
-					$window
-						.on('orientationchange', function() {
-
-							if (!isLocked)
-								$body._reposition();
-
-						});
-
-					if (skel.vars.IEVersion < 9)
-						$window
-							.on('resize', function() {
-								$wrapper.css('min-height', $window.height());
-							});
-
-				// CSS polyfills (IE<9).
-					if (skel.vars.IEVersion < 9)
-						$(':last-child').addClass('last-child');
-
-				// Init.
-					$window
-						.trigger('resize');
-
-					if (hash && hash in panels)
-						panels[hash]._activate(true);
-
-					$wrapper.fadeTo(400, 1.0);
-
-			})
-			.on('-desktop', function() {
-
-				window.setTimeout(function() {
-					location.reload(true);
-				}, 50);
-
-			});
-
+	// Play initial animations on page load.
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
 	});
+
+	// Nav.
+	$nav_links
+		.on('click', function(event) {
+
+			var href = $(this).attr('href');
+
+			// Not a panel link? Bail.
+				if (href.charAt(0) != '#'
+				||	$panels.filter(href).length == 0)
+					return;
+
+			// Prevent default.
+				event.preventDefault();
+				event.stopPropagation();
+
+			// Change panels.
+				if (window.location.hash != href)
+					window.location.hash = href;
+
+		});
+
+// Panels.
+
+	// Initialize.
+		(function() {
+
+			var $panel, $link;
+
+			// Get panel, link.
+				if (window.location.hash) {
+
+					$panel = $panels.filter(window.location.hash);
+					$link = $nav_links.filter('[href="' + window.location.hash + '"]');
+
+				}
+
+			// No panel/link? Default to first.
+				if (!$panel
+				||	$panel.length == 0) {
+
+					$panel = $panels.first();
+					$link = $nav_links.first();
+
+				}
+
+			// Deactivate all panels except this one.
+				$panels.not($panel)
+					.addClass('inactive')
+					.hide();
+
+			// Activate link.
+				$link
+					.addClass('active');
+
+			// Reset scroll.
+				$window.scrollTop(0);
+
+		})();
+
+		// Hashchange event.
+		$window.on('hashchange', function(event) {
+
+			var $panel, $link;
+
+			// Get panel, link.
+				if (window.location.hash) {
+
+					$panel = $panels.filter(window.location.hash);
+					$link = $nav_links.filter('[href="' + window.location.hash + '"]');
+
+					// No target panel? Bail.
+						if ($panel.length == 0)
+							return;
+
+				}
+
+			// No panel/link? Default to first.
+				else {
+					$panel = $panels.first();
+					$link = $nav_links.first();
+				}
+
+			// Deactivate all panels.
+				$panels.addClass('inactive');
+
+			// Deactivate all links.
+				$nav_links.removeClass('active');
+
+			// Activate target link.
+				$link.addClass('active');
+
+			// Set max/min height.
+				$main
+					.css('max-height', $main.height() + 'px')
+					.css('min-height', $main.height() + 'px');
+
+			// Delay.
+				setTimeout(function() {
+
+					// Hide all panels.
+						$panels.hide();
+
+					// Show target panel.
+						$panel.show();
+
+					// Set new max/min height.
+						$main
+							.css('max-height', $panel.outerHeight() + 'px')
+							.css('min-height', $panel.outerHeight() + 'px');
+
+					// Reset scroll.
+						$window.scrollTop(0);
+
+					// Delay.
+						window.setTimeout(function() {
+
+							// Activate target panel.
+								$panel.removeClass('inactive');
+
+							// Clear max/min height.
+								$main
+									.css('max-height', '')
+									.css('min-height', '');
+
+							// IE: Refresh.
+								$window.triggerHandler('--refresh');
+
+							// Unlock.
+								locked = false;
+
+						}, 500);
+
+				}, 250);
+
+		});
 
 })(jQuery);
